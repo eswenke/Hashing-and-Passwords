b81ea8f7905b3eed0b438e754ad02e30f9f003dc
eswenke <swenke.ethan.us@gmail.com>
2024-02-08 23:42:34 -0800

    working task2, parallelized

--- task2.py
+++ task2.py
@@ -2,24 +2,17 @@
 # Ethan Swenke and HanYu Wu
 # CSC-321-03
 
-# STEPS:
-    # take the bcrypt file and collect each users salt and hash
-    # recommended to split hashses up in groups based on workfactor
-    # for all 6 to 10 letter words in the words.words() list:
-        # checkpw() args are IN BYTES i.e. b"<insert arg here>" 
-        # checkpw() takes in the word as the first param, then the entire hash as the second param
-        # if checkpw() returns true, the password has been cracked
-
 from bcrypt import *
 from nltk.corpus import words
+import time
 
 
 def read_shadow():
     try:
         with open("shadow.txt", 'r') as file:
-            for line in file:
-                brute_force(line)
-                break
+            lines = file.read().splitlines()
+            for line in lines:
+                print(brute_force(line))
     except FileNotFoundError:
         print(f"File 'shadow.txt' not found.")
     except Exception as e:
@@ -29,23 +22,28 @@ def read_shadow():
 def brute_force(line):
     # for each word in the words list
         # run checkpq(b"word", b"hash") for the given user past the colon
-    
+
+    start_time = time.time() 
     words = get_words()
     user = line[:line.find(':')]
-    user_hash = bytes(line[(line.find(':')+1):])
+    user_hash = line[(line.find(':')+1):].encode('utf-8')
+
     for word in words:
-        if checkpw(bytes(word), user_hash):
+        if checkpw(word.encode('utf-8'), user_hash):
+            end_time = time.time()  # Record the end time
+            elapsed_time = end_time - start_time
             print(user + "'s password: " + word)
-            print(user + "'s hash: " + str(user_hash))
-            return
+            print("crack time: " + str(elapsed_time))
+            return True
+    return False
 
 
 def get_words():
     # filter words list down to words of only 6 to 10 letters
 
-    words = words.words()
-    filtered_words = list(filter(lambda x: 6 <= len(x) <= 10, words))
+    valid_words = words.words()
+    filtered_words = list(filter(lambda x: 6 <= len(x) <= 10, valid_words))
     return filtered_words
 
 if __name__ == "__main__":
-    read_shadow()
+    print(read_shadow())
